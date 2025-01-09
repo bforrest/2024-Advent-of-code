30de2ab8d756a58650a610480cd78f68c9fbb41b
Barry Forrest <barry.forrest@gmail.com>
2025-01-09 15:01:10 -0600

    Day 5 wip

---
+++ 2024 Advent of Code/Day5/Puzzle.cs
@@ -0,0 +1,103 @@
+using System.Net;
+using System.Reflection;
+using System.Security;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Abstractions;
+
+namespace _2024_Advent_of_Code.Day5;
+
+public class Puzzle
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Puzzle
+        (ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    [Fact]
+    public Task Sample1()
+    {
+        var input = Sample;
+        var (rules, updates) = ParseInput(input);
+
+        foreach (var update in updates)
+        {
+            _testOutputHelper.WriteLine("Update: " + update);
+            // split to array
+            // for each number, check if it is in the rules
+            // see if order matches the rule(s)
+            var pages = update.Split(",");
+
+            foreach (var item in pages)
+            {
+                var matchingRule = rules.Where(x => x.StartsWith(item));
+                foreach (var rule in matchingRule)
+                {
+                    _testOutputHelper.WriteLine($"Matching rule: {rule}");
+                }
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private (List<string> rules, List<string> updates) ParseInput(string[] input)
+    {
+        var rules = new List<string>();
+        var updates = new List<string>();
+        
+        foreach (var line in input)
+        {
+            if(string.IsNullOrWhiteSpace(line)) continue;
+            
+            if(line.Contains("|")) rules.Add(line);
+
+            if(line.Contains(",")) updates.Add(line);
+        }
+
+        _testOutputHelper.WriteLine($"rule count: {rules.Count}");
+        _testOutputHelper.WriteLine($"update count: {updates.Count}");
+        return (rules, updates);
+    }
+
+    private string[] ReadInput()
+    {
+        var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Day4", "input.txt");
+        return File.ReadAllLines(path);
+    }
+
+    private string[] Sample = new string[]
+    {
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47"
+    };
+}
